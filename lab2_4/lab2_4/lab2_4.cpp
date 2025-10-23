b0ea8bb9750b10507043c43ead4b03f968f93d72
David <spartandav_disk@mail.ru>
2025-10-23 11:52:48 +0300

    Create lab2_4.cpp

---
+++ lab2_4/lab2_4/lab2_4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "Header.h"
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define summm(a, b) a + b;
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    cout << MySpace::func1(10) << '\n';
+    cout << MySpace::func1(10) << '\n';
+    cout << MySpace::func1(80) << '\n';
+    cout << summm(34, 6);
+    cout << '\n';
+    cout << "Для закрытия консоли, введите любой символ и нажмите Enter";
+    int a = 0;
+    cin >> a;
+
+}
